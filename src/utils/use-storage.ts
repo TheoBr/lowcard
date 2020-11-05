09b0579423039506545235d71231f640383987b7
theo <me@t3.gg>
2020-11-05 15:36:35 -0800

    type cleanup

--- src/utils/use-storage.ts
+++ src/utils/use-storage.ts
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-type UpdateStoreType<T> = (stored: T) => T;
+type UpdateStoreType<T> = (value: T | ((stored: T) => T)) => void;
 
 export function useLocalStorage<T>(
   key: string,
   initialValue: T
-): [T, (value: UpdateStoreType<T>) => void] {
+): [T, UpdateStoreType<T>] {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
@@ -23,7 +23,7 @@ export function useLocalStorage<T>(
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = (value: UpdateStoreType<T>) => {
+  const setValue: UpdateStoreType<T> = (value) => {
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
